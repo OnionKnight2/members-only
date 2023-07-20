0ce7ef9787d5dbdcd4b76fc31874aa0aac1f94ae
rastko <vucen.rastko@gmail.com>
2023-07-20 23:06:46 +0200

    Add logic to only see user's name when signed in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
+  before_action :find_user, only: [:index, :create]
+  before_action :authenticate_user!, only: [:new, :create]
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   # GET /posts/new
@@ -12,10 +14,11 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user_id = @user.id
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to posts_url, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -28,5 +31,9 @@ class PostsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :body)
+    end
+
+    def find_user
+      @user = current_user
     end
 end
